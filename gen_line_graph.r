5b8e36f5d13e3526438ca3299069256754251e4c
Raj Agrawal <r.agrawal@csail.mit.edu>
2018-05-25 16:36:10 -0400

    Create gen_line_graph.r
    code to generate line graphs

---
+++ gen_line_graph.r
@@ -0,0 +1,16 @@
+library(bnlearn)
+
+all_line_graphs = function(p){
+  all_dags = list()
+  e = empty.graph(as.character(1:p))
+  arc_vec = rep(as.character(1:p), each=2)
+  arc_vec = arc_vec[2:(2 * p - 1)]
+  arc_set = matrix(arc_vec, ncol = 2, byrow = TRUE)
+  arcs(e) = arc_set
+  all_dags[[1]] = e
+  for(i in 1:(p-1)){
+    e = reverse.arc(e, as.character(i), as.character(i + 1))
+    all_dags[[i]] = e
+  }
+  return(all_dags)
+}
