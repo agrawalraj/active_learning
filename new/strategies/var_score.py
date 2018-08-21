80581e3b8687350b12709154fa10e584e4a5078c
Raj <raj@Rajs-MacBook-Pro-3.local>
2018-08-20 23:28:21 -0400

    fixed more bugs

--- new/strategies/var_score.py
+++ new/strategies/var_score.py
@@ -14,13 +14,12 @@ def node_iv_var_mat(adj_mat, node_vars, iv_strengths, n_monte_carlo=1000):
     var_mat = np.zeros((p, p))
     ivs = []
     for i, iv_strength in enumerate(iv_strengths):
-        print('FIXXXXXX THISSS')
-        # ivs.append(BinaryIntervention(iv_strength, -iv_strength))
-        ivs.append(ConstantIntervention(iv_strength))
+        ivs.append(BinaryIntervention(ConstantIntervention(val=iv_strength).sample,
+         ConstantIntervention(val=-iv_strength).sample))
     for iv in range(p):
         iv_fn = ivs[iv]
         iv_samps = gdag.sample_interventional({iv: iv_fn.sample}, n_monte_carlo)
-        var_mat[iv, :] = np.multiply(np.var(iv_samps, axis=0), 1 / node_vars)
+        var_mat[:, iv] = np.multiply(np.var(iv_samps, axis=0), 1 / node_vars)
     return var_mat
 
 
@@ -30,12 +29,12 @@ def get_orient_mask(target, adj_mat):
     parents_mask = parents_mask != 0
     dag = cd.DAG.from_amat(adj_mat)
     cpdag = dag.cpdag() 
-    for parent, parent_mask in enumerate(parents_mask):
-        for iv in range(adj_mat.shape[0]):
-            if parent_mask != False:
-                icpdag = dag.interventional_cpdag([iv], cpdag=cpdag)
-                oriented_parents = [p for p in dag.parents[target] if (p, target) in (icpdag.arcs - cpdag.arcs)]
-                if iv in oriented_parents:
+    for iv in range(adj_mat.shape[0]):
+        if iv != target:
+            icpdag = dag.interventional_cpdag([iv], cpdag=cpdag)
+            oriented_parents = [p for p in dag.parents[target] if (p, target) in (icpdag.arcs - cpdag.arcs)]
+            for parent, parent_mask in enumerate(parents_mask):
+                if parent_mask != False and parent in oriented_parents:
                     mask[parent, iv] = 1
     return mask
 
@@ -67,6 +66,10 @@ def create_var_score_fn(parent_shrinkage_scores, target, adj_mats, node_vars, iv
 
 def greedy_iv(int_score_fn, int_set, K):
     pass
+    # int_score_fn - function that returns float from list of interventions
+    # int_set - list of possible interventions
+    # K - max number of interventions 
+    # returns - K top scoring interventions by maximizing int_score_fn
 
 
 def create_variance_strategy(target, iv_strengths, n_boot=100):
@@ -97,8 +100,21 @@ def create_variance_strategy(target, iv_strengths, n_boot=100):
         print(parent_probs)
         parent_shrinkage_scores = {p: graph_utils.probability_shrinkage(prob) for p, prob in parent_probs.items()}
         var_score_fn = create_var_score_fn(parent_shrinkage_scores, target, adj_mats, iv_strengths)
-        interventions = greedy_iv(var_score_fn, iteration_data.max_interventions)
+        p = amats[0].shape[0]
+        int_set = [i for i in range(p) if p != target]
+        interventions = greedy_iv(var_score_fn, int_set, iteration_data.max_interventions)
         return interventions
     return variance_strategy
+
+
+if __name__ == '__main__':
+    # B = np.zeros((3, 3))
+    # B[0, 2] = 1
+    # B[1, 2] = 1
+    # B[0, 1] = 2
+    # gdag = cd.GaussDAG.from_weight_matrix(B)
+    # obs_samps = gdag.sample(10000)
+    # node_vars = np.var(obs_samps, axis=0) # exact is (1, 5, 11)
+    # iv_strengths = [2, 3, 1]
 
 
