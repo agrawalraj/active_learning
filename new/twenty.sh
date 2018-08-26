f218a1e2936f0878220da879b574c504852d72b3
csquires <chandlersquires18@gmail.com>
2018-08-26 13:44:05 -0400

    redo script for datasets

--- new/twenty.sh
+++ new/twenty.sh
@@ -1,8 +1,42 @@
 #!/usr/bin/env bash
 
+python3 make_dataset.py -p 20 -s .5 -d 50 --folder twenty
+python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 12 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 12 -b 3 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 12 -b 3 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 12 -b 4 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 12 -b 4 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 12 -b 4 -k 2 --folder twenty --strategy learn-parents
+
+python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 30 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 30 -b 3 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 30 -b 3 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 30 -b 4 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 30 -b 4 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 30 -b 4 -k 2 --folder twenty --strategy learn-parents
+
+python3 run_experiments.py -n 60 -b 2 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 60 -b 2 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 60 -b 2 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 60 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 60 -b 3 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 60 -b 3 -k 2 --folder twenty --strategy learn-parents
 python3 run_experiments.py -n 60 -b 4 -k 2 --folder twenty --strategy random
 python3 run_experiments.py -n 60 -b 4 -k 2 --folder twenty --strategy edge-prob
 python3 run_experiments.py -n 60 -b 4 -k 2 --folder twenty --strategy learn-parents
+
+python3 run_experiments.py -n 120 -b 2 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 120 -b 2 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 120 -b 2 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 120 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 120 -b 3 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 120 -b 3 -k 2 --folder twenty --strategy learn-parents
 python3 run_experiments.py -n 120 -b 4 -k 2 --folder twenty --strategy random
 python3 run_experiments.py -n 120 -b 4 -k 2 --folder twenty --strategy edge-prob
 python3 run_experiments.py -n 120 -b 4 -k 2 --folder twenty --strategy learn-parents
@@ -17,24 +51,5 @@ python3 run_experiments.py -n 240 -b 4 -k 2 --folder twenty --strategy random
 python3 run_experiments.py -n 240 -b 4 -k 2 --folder twenty --strategy edge-prob
 python3 run_experiments.py -n 240 -b 4 -k 2 --folder twenty --strategy learn-parents
 
-python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy edge-prob
-python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy learn-parents
-python3 run_experiments.py -n 30 -b 3 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 30 -b 3 -k 2 --folder twenty --strategy edge-prob
-python3 run_experiments.py -n 30 -b 3 -k 2 --folder twenty --strategy learn-parents
-python3 run_experiments.py -n 30 -b 4 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 30 -b 4 -k 2 --folder twenty --strategy edge-prob
-python3 run_experiments.py -n 30 -b 4 -k 2 --folder twenty --strategy learn-parents
 
-python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy edge-prob
-python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy learn-parents
-python3 run_experiments.py -n 12 -b 3 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 12 -b 3 -k 2 --folder twenty --strategy edge-prob
-python3 run_experiments.py -n 12 -b 3 -k 2 --folder twenty --strategy learn-parents
-python3 run_experiments.py -n 12 -b 4 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 12 -b 4 -k 2 --folder twenty --strategy edge-prob
-python3 run_experiments.py -n 12 -b 4 -k 2 --folder twenty --strategy learn-parents
 
-tar -czf twenty.tar.gz twenty
