dda2e99604914a007bc644e7997f7b19576081bf
csquires <chandlersquires18@gmail.com>
2018-08-26 01:13:00 -0400

    modify twenty.sh script

--- new/twenty.sh
+++ new/twenty.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-python3 make_dataset.py -p 20 -s .5 -d 50 --folder twenty
-python3 run_experiments.py -n 60 -b 2 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 60 -b 2 -k 2 --folder twenty --strategy edge-prob
-python3 run_experiments.py -n 60 -b 2 -k 2 --folder twenty --strategy learn-parents
-python3 run_experiments.py -n 120 -b 2 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 120 -b 2 -k 2 --folder twenty --strategy edge-prob
-python3 run_experiments.py -n 120 -b 2 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 60 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 60 -b 3 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 60 -b 3 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 120 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 120 -b 3 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 120 -b 3 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 60 -b 4 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 60 -b 4 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 60 -b 4 -k 2 --folder twenty --strategy learn-parents
+python3 run_experiments.py -n 120 -b 4 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 120 -b 4 -k 2 --folder twenty --strategy edge-prob
+python3 run_experiments.py -n 120 -b 4 -k 2 --folder twenty --strategy learn-parents
