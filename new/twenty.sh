cbdceeea41fce7070eb6dbda1f3e55f7101323e9
Ubuntu <ubuntu@ip-172-31-90-227.ec2.internal>
2018-08-27 23:57:38 +0000

    change to complete graph

--- new/twenty.sh
+++ new/twenty.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python3 make_dataset.py -p 20 -s .5 -d 50 -t erdos --folder twenty
+python3 make_dataset.py -p 20 -s 1 -d 50 -t erdos --folder twenty
 python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy random
 python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy edge-prob
 python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy learn-parents
