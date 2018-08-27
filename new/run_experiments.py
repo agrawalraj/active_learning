fix variance score folders

new/run_experiments.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import numpy as np
 from strategies.simulator import SimulationConfig, simulate
-from strategies import random_nodes, learn_target_parents, edge_prob
+from strategies import random_nodes, learn_target_parents, edge_prob, var_score
 from config import DATA_FOLDER
 import causaldag as cd
 from multiprocessing import Pool, cpu_count
@@ -37,10 +37,23 @@ dags = [cd.GaussDAG.from_amat(amat) for amat in amats]
 nnodes = len(dags[0].nodes)
 target = int(np.ceil(nnodes/2))
 
+
+def get_strategy(strategy, dag):
+    if strategy == 'random':
+        return random_nodes.random_strategy
+    if strategy == 'learn-parents':
+        return learn_target_parents.create_learn_target_parents(target, NUM_BOOTSTRAP_DAGS_BATCH)
+    if strategy == 'edge-prob':
+        return edge_prob.create_edge_prob_strategy(target, NUM_BOOTSTRAP_DAGS_BATCH)
+    if strategy == 'var-score':
+        node_vars = np.diag(dag.covariance)
+        return var_score.create_variance_strategy(target, node_vars, [2]*len(node_vars))
+
 STRATEGIES = {
     'random': random_nodes.random_strategy,
     'learn-parents': learn_target_parents.create_learn_target_parents(target, NUM_BOOTSTRAP_DAGS_BATCH),
-    'edge-prob': edge_prob.create_edge_prob_strategy(target, NUM_BOOTSTRAP_DAGS_BATCH)
+    'edge-prob': edge_prob.create_edge_prob_strategy(target, NUM_BOOTSTRAP_DAGS_BATCH),
+    'var-score': var_score.create_variance_strategy(target, )
 }
 
 folders = [
@@ -56,5 +69,4 @@ def simulate_(tup):
 
 with Pool(cpu_count()-1) as p:
     p.map(simulate_, zip(dags, folders))
-
 

new/strategies/var_score.py
@@ -90,11 +90,16 @@ def create_variance_strategy(target, node_vars, iv_strengths, n_boot=100):
         if int(n) != n:
             raise ValueError('n_samples / (n_batches * max interventions) must be an integer')
 
+        # === DEFINE PATHS FOR FILES WHICH WILL HOLD THE TEMPORARY DATA
+        samples_path = os.path.join(iteration_data.batch_folder, 'samples.csv')
+        interventions_path = os.path.join(iteration_data.batch_folder, 'interventions.csv')
+        dags_path = os.path.join(iteration_data.batch_folder, 'TEMP_DAGS/')
+
         # === SAVE DATA, THEN CALL R CODE WITH DATA TO GET DAG SAMPLES
         print('intervened nodes:', iteration_data.current_data.keys())
-        graph_utils._write_data(iteration_data.current_data)
-        graph_utils.run_gies_boot(n_boot, config.TEMP_SAMPLES_PATH, config.TEMP_INTERVENTIONS_PATH, delete=True)
-        amats, dags = graph_utils._load_dags()
+        graph_utils._write_data(iteration_data.current_data, samples_path, interventions_path)
+        graph_utils.run_gies_boot(n_boot, samples_path, interventions_path, dags_path, delete=True)
+        amats, dags = graph_utils._load_dags(dags_path, delete=True)
         dag_target_parents = [dag.parents[target] for dag in dags]
         if len(dags) != n_boot:
             raise RuntimeError('Correct number of DAGs not saved, check R code')
@@ -108,7 +113,6 @@ def create_variance_strategy(target, node_vars, iv_strengths, n_boot=100):
             for p in target_parents:
                 parent_counts[p] += 1
         parent_probs = {p: c/len(dags) for p, c in parent_counts.items()}
-        print(parent_probs)
         parent_shrinkage_scores = {p: graph_utils.probability_shrinkage(prob) for p, prob in parent_probs.items()}
         var_score_fn = create_var_score_fn(parent_shrinkage_scores, target, adj_mats, node_vars, iv_strengths)
         p = amats[0].shape[0]
@@ -122,7 +126,7 @@ def create_variance_strategy(target, node_vars, iv_strengths, n_boot=100):
     return variance_strategy
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # B = np.zeros((3, 3))
     # B[0, 2] = 1
     # B[1, 2] = 1