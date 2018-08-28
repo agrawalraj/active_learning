f9d52d5e7144b364fd23962ab0cb501dc2a0ea9c
csquires <chandlersquires18@gmail.com>
2018-08-27 23:46:41 -0400

    remove a print and change back to n=24

--- new/twenty.sh
+++ new/twenty.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 python3 make_dataset.py -p 20 -s .5 -d 50 -t erdos --folder twenty
-python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 12 -b 2 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 12 -b 3 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 12 -b 3 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 12 -b 4 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 12 -b 4 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 24 -b 2 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 24 -b 2 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 24 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 24 -b 3 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 24 -b 4 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 24 -b 4 -k 2 --folder twenty --strategy var-score
 
 python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy random
 python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy var-score
