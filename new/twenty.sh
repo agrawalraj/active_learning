511a953bd880d3cf2c7d632eff565e3d3b08609b
csquires <chandlersquires18@gmail.com>
2018-08-28 00:51:24 -0400

    change sample sizes

--- new/twenty.sh
+++ new/twenty.sh
@@ -8,33 +8,33 @@ python3 run_experiments.py -n 24 -b 3 -k 2 --folder twenty --strategy var-score
 python3 run_experiments.py -n 24 -b 4 -k 2 --folder twenty --strategy random
 python3 run_experiments.py -n 24 -b 4 -k 2 --folder twenty --strategy var-score
 
-python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 30 -b 2 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 30 -b 3 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 30 -b 3 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 30 -b 4 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 30 -b 4 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 48 -b 2 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 48 -b 2 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 48 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 48 -b 3 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 48 -b 4 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 48 -b 4 -k 2 --folder twenty --strategy var-score
 
-python3 run_experiments.py -n 60 -b 2 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 60 -b 2 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 60 -b 3 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 60 -b 3 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 60 -b 4 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 60 -b 4 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 96 -b 2 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 96 -b 2 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 96 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 96 -b 3 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 96 -b 4 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 96 -b 4 -k 2 --folder twenty --strategy var-score
 
-python3 run_experiments.py -n 120 -b 2 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 120 -b 2 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 120 -b 3 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 120 -b 3 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 120 -b 4 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 120 -b 4 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 192 -b 2 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 192 -b 2 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 192 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 192 -b 3 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 192 -b 4 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 192 -b 4 -k 2 --folder twenty --strategy var-score
 
-python3 run_experiments.py -n 240 -b 2 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 240 -b 2 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 240 -b 3 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 240 -b 3 -k 2 --folder twenty --strategy var-score
-python3 run_experiments.py -n 240 -b 4 -k 2 --folder twenty --strategy random
-python3 run_experiments.py -n 240 -b 4 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 384 -b 2 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 384 -b 2 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 384 -b 3 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 384 -b 3 -k 2 --folder twenty --strategy var-score
+python3 run_experiments.py -n 384 -b 4 -k 2 --folder twenty --strategy random
+python3 run_experiments.py -n 384 -b 4 -k 2 --folder twenty --strategy var-score
 
 tar -czf data/twenty.tar.gz data/twenty
 
