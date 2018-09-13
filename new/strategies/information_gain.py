02d96f86f79236be3842e954d4b44acde6313ac0
csquires <chandlersquires18@gmail.com>
2018-09-13 15:14:19 -0400

    work on info-gain strategy and try small world network

--- new/strategies/information_gain.py
+++ new/strategies/information_gain.py
@@ -1,18 +1,42 @@
 from strategies.collect_dags import collect_dags
 from utils import graph_utils
+import xarray as xr
+import numpy as np
+import itertools as itr
 
 
-def create_info_gain_strategy(n_boot):
+def create_info_gain_strategy(precision_matrix, n_boot):
     def info_gain_strategy(iteration_data):
         # === CALCULATE NUMBER OF SAMPLES IN EACH INTERVENTION
-        n = iteration_data.n_samples / (iteration_data.n_batches * iteration_data.max_interventions)
-        if int(n) != n:
+        nsamples = iteration_data.n_samples / (iteration_data.n_batches * iteration_data.max_interventions)
+        if int(nsamples) != nsamples:
             raise ValueError('n_samples / (n_batches * max interventions) must be an integer')
+        nnodes = precision_matrix.shape[0]
 
         sampled_dags = collect_dags(iteration_data.batch_folder, iteration_data.current_data, n_boot)
-        # TODO estimate prec from iteration_data.current_data ???
-        gauss_dags = [graph_utils.prec2dag(prec, dag.topological_sort()) for dag in sampled_dags]
-        # TODO take n_samples/n_batches samples for each intervention
+        gauss_dags = [graph_utils.prec2dag(precision_matrix, dag.topological_sort()) for dag in sampled_dags]
+        dag_datasets = xr.DataArray(
+            np.zeros([n_boot, nnodes, nsamples]),
+            dims=['dag', 'intervened_node', 'sample'],
+            coords={
+                'dag': list(range(n_boot)),
+                'intervened_node': list(range(nnodes)),
+                'sample': list(range(nsamples))
+            }
+        )
+        for (i, gauss_dag), p in itr.product(enumerate(gauss_dags), range(nnodes)):
+            dag_datasets.loc[dict(dag=i, intervened_node=p)] = iteration_data.interventions[p].sample(nsamples)
+
+        log_probabilities = xr.DataArray(
+            np.zeros([n_boot, n_boot, nnodes, nsamples]),
+            dims=['generating_dag', 'dag', 'intervened_node', 'sample'],
+            coords={
+                'generating_dag': list(range(n_boot)),
+                'dag': list(range(n_boot)),
+                'intervened_node': list(range(nnodes)),
+                'sample': list(range(nsamples)),
+            }
+        )
 
         # TODO calculate tensor of log probabilities
 
