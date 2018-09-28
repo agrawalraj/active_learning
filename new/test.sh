99564ee695731b1b6dc04556df30b1b2b75e62d1
csquires <chandlersquires18@gmail.com>
2018-09-28 08:18:39 -0400

    modify info gain to average logpdf over multiple datapoints

--- new/test.sh
+++ new/test.sh
@@ -3,4 +3,4 @@
 #!/usr/bin/env bash
 
 python3 make_dataset.py -p 5 -s 1 -d 1 -t erdos --folder test
-python3 run_experiments.py -n 32 -b 1 -k 2 --boot 50 -s 2 --folder test --strategy entropy-enum
+python3 run_experiments.py -n 32 -b 1 -k 2 --boot 50 -s 2 --folder test --strategy entropy
