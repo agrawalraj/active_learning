113142866e38fe9392890b233065e262211bb84b
Ubuntu <ubuntu@ip-172-31-92-4.ec2.internal>
2018-10-03 20:51:25 +0000

    more updates to server

--- new/setup-server.sh
+++ new/setup-server.sh
@@ -3,6 +3,7 @@
 yes| sudo apt-get install libv8-3.14-dev
 yes| sudo apt-get install libcurl4-openssl-dev
 yes | sudo apt-get install r-base
+yes | sudo apt-get install graphviz libgraphviz-dev pkg-config
 Rscript install.R
 sudo apt install python3-pip
 pip3 install virtualenv
