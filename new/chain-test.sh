3068b0b8f644e0adca80201bff5ef8a3f5c1422b
csquires <chandlersquires18@gmail.com>
2018-10-02 22:53:27 -0400

    make verbose

--- new/chain-test.sh
+++ new/chain-test.sh
@@ -2,6 +2,6 @@
 
 python3 make_dataset.py -p 11 -d 1 -t chain --folder chain_test10
 
-python3 run_experiments.py -n 150 -b 3 -k 1 -m 10 -s .1 -i gauss --folder chain_test10 --strategy entropy-dag-collection
+python3 run_experiments.py -n 150 -b 3 -k 1 -m 10 -s .1 -i --verbose gauss --folder chain_test10 --strategy entropy-dag-collection
 python3 run_experiments.py -n 150 -b 3 -k 1 -s .1 -i gauss --folder chain_test10 --strategy random
 
