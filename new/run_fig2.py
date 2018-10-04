62d1b087d11eb008fbe3e86a4a4c8cdb2165b79a
csquires <chandlersquires18@gmail.com>
2018-10-04 09:07:26 -0400

    run again but actually get samples in unbounded case

--- new/run_fig2.py
+++ new/run_fig2.py
@@ -2,17 +2,26 @@ import os
 import itertools as itr
 
 ns = [24, 48, 96, 192]
-ks = [1, None]
+ks = [None]
 bs = [1, 2, 3]
 
 p = 25
 s = .25
 ndags = 50
 
-os.system(f'python3 make_dataset.py -p {p} -s {.25} -d {ndags} -t erdos-bounded --folder erdos_renyi_final')
+# os.system(f'python3 make_dataset.py -p {p} -s {.25} -d {ndags} -t erdos-bounded --folder erdos_renyi_final')
 for n, b, k in itr.product(ns, bs, ks):
-    os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection')
+    if k is None:
+        os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection')
+    else:
+        os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection')
 for n, b, k in itr.product(ns, bs, ks):
-    os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random')
+    if k is None:
+        os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random')
+    else:
+        os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random')
 for n, b, k in itr.product(ns, bs, ks):
-    os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random-smart')
+    if k is None:
+        os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random-smart')
+    else:
+        os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random-smart')
