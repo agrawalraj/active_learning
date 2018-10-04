39907ddd9683f93c46501accc65214e480a81705
csquires <chandlersquires18@gmail.com>
2018-10-04 06:17:58 -0400

    run all entropy-dag-collections first

--- new/run_fig2.py
+++ new/run_fig2.py
@@ -12,5 +12,7 @@ ndags = 50
 os.system(f'python3 make_dataset.py -p {p} -s {.25} -d {ndags} -t erdos-bounded --folder erdos_renyi_final')
 for n, b, k in itr.product(ns, bs, ks):
     os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection')
+for n, b, k in itr.product(ns, bs, ks):
     os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random')
+for n, b, k in itr.product(ns, bs, ks):
     os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random-smart')
