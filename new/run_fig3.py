ac86627bd55d075bfa5bd2e7b365acf7e36faef9
csquires <chandlersquires18@gmail.com>
2018-10-05 01:22:45 -0400

    run other ns for fig3

--- new/run_fig3.py
+++ new/run_fig3.py
@@ -1,7 +1,7 @@
 import os
 import itertools as itr
 
-ns = [24, 48, 96, 192]
+ns = [12, 36]
 ks = [1]
 bs = [1, 2]
 
@@ -9,7 +9,7 @@ p = 25
 s = .25
 ndags = 50
 
-os.system(f'python3 make_dataset.py -p {p} -s {.25} -d {ndags} -t unoriented_by_one --folder erdos_renyi_descendant_new')
+# os.system(f'python3 make_dataset.py -p {p} -s {.25} -d {ndags} -t unoriented_by_one --folder erdos_renyi_descendant_new')
 for n, b, k in itr.product(ns, bs, ks):
     if k is None:
         os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --target-allowed 0 --folder erdos_renyi_descendant_new --strategy entropy-dag-collection')
