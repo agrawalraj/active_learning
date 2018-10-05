98d41be659c9ace483074c6f5128ff55fbf445d0
csquires <chandlersquires18@gmail.com>
2018-10-04 23:42:23 -0400

    update run_fig4

--- new/run_fig4.py
+++ new/run_fig4.py
@@ -2,12 +2,12 @@ import os
 import itertools as itr
 
 ns = [24, 48, 96, 192]
-ks = [1, None]
+ks = [1]
 bs = [1, 2]
 
 p = 11
 s = .25
-ndags = 50
+ndags = 25
 
 os.system(f'python3 make_dataset.py -p {p} -s {.25} -d {ndags} -t erdos-bounded --folder erdos_renyi_gies')
 for n, b, k in itr.product(ns, bs, ks):
@@ -15,6 +15,11 @@ for n, b, k in itr.product(ns, bs, ks):
         os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --target-allowed 0 --folder erdos_renyi_gies --strategy entropy')
     else:
         os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --target-allowed 0 --folder erdos_renyi_gies --strategy entropy')
+for n, b, k in itr.product(ns, bs, ks):
+    if k is None:
+        os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --target-allowed 0 --folder erdos_renyi_gies --strategy entropy-dag-collection')
+    else:
+        os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --target-allowed 0 --folder erdos_renyi_gies --strategy entropy-dag-collection')
 for n, b, k in itr.product(ns, bs, ks):
     if k is None:
         os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --target-allowed 0 --folder erdos_renyi_gies --strategy random-smart')
