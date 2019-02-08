d5b3edffdd6409a3c4b1188262c54c2a21c9298f
csquires <chandlersquires18@gmail.com>
2019-02-08 12:10:49 -0500

    modify run_fig2 to use slurm

--- new/run_fig2.py
+++ new/run_fig2.py
@@ -8,24 +8,31 @@ bs = [1, 2, 3]
 p = 25
 s = .25
 ndags = 50
+strategies = {'bed', 'entropy', 'random', 'random-smart'}
 
 os.system(f'python3 make_dataset.py -p {p} -s {.25} -d {ndags} -t erdos-bounded --folder erdos_renyi_final')
-for n, b, k in itr.product(ns, bs, ks):
-    if k is None:
-        os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy budgeted_exp_design')
-    else:
-        os.system(f'python3 run_experiments.py -n {n} -b 1 -k {b*k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy budgeted_exp_design')
-    # if k is None:x
-    #     os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection')
-    # else:
-    #     os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection')
-# for n, b, k in itr.product(ns, bs, ks):
-#     if k is None:
-#         os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random')
-#     else:
-#         os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random')
-# for n, b, k in itr.product(ns, bs, ks):
-#     if k is None:
-#         os.system(f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random-smart')
-#     else:
-#         os.system(f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random-smart')
+for n, b, k, strat in itr.product(ns, bs, ks, strategies):
+    if 'bed' == strat:
+        if k is None:
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy budgeted_exp_design'
+        else:
+            cmd = f'python3 run_experiments.py -n {n} -b 1 -k {b*k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy budgeted_exp_design'
+    if 'entropy' == strat:
+        if k is None:
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection'
+        else:
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection'
+    if 'random' == strat:
+        if k is None:
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random'
+        else:
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random'
+    if 'random-smart' == strat:
+        if k is None:
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random-smart'
+        else:
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random-smart'
+    os.system(f'echo "{cmd}" > tmp.sh')
+    os.system('cat slurm_template.sh tmp.sh > job.sh')
+    os.system('rm tmp.sh')
+    os.system('sbatch job.sh')
---
+++ new/slurm-template.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 32
+#SBATCH --time 06:00:00
+#SBATCH -p newnodes
+#SBATCH -J experiment_design
+
+module add engaging/python/3.6.0
+source venv/bin/activate
+
