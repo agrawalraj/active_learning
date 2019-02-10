f8f1f8c833b2313900d6c769273165ace5e05e32
csquires <chandlersquires18@gmail.com>
2019-02-10 11:37:17 -0500

    add multiple mec

--- new/run_fig6.py
+++ new/run_fig6.py
@@ -14,14 +14,14 @@ os.system(f'python3 make_dataset.py -p {p} -s {.25} -d {ndags} -t erdos-bounded 
 for n, b, k, strat in itr.product(ns, bs, ks, strategies):
     if 'entropy-multiple-mec' == strat:
         if k is None:
-            cmd = f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection-multiple-mec'
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_multiple_mec --strategy entropy-dag-collection-multiple-mec'
         else:
-            cmd = f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy entropy-dag-collection-multiple-mec'
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_multiple_mec --strategy entropy-dag-collection-multiple-mec'
     if 'random' == strat:
         if k is None:
-            cmd = f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random'
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -m 2 -s .1 -i gauss --folder erdos_renyi_multiple_mec --strategy random'
         else:
-            cmd = f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_final --strategy random'
+            cmd = f'python3 run_experiments.py -n {n} -b {b} -k {k} -m 2 -s .1 -i gauss --folder erdos_renyi_multiple_mec --strategy random'
     os.system(f'echo "{cmd}" > tmp.sh')
     os.system('cat slurm_template.sh tmp.sh > job.sh')
     os.system('rm tmp.sh')
